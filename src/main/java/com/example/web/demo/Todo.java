579bc302b041f99fdf3679f210f5ad3e2616b9dc
Rainer Bieniek <Rainer.Bieniek@cumulus-cloud-consulting.de>
2022-01-14 12:20:16 +0100

    Initial check-in

---
+++ src/main/java/com/example/web/demo/Todo.java
@@ -0,0 +1,8 @@
+package com.example.web.demo;
+
+import lombok.Data;
+
+@Data
+public class Todo {
+    private final String description;
+}
---
+++ src/main/resources/static/js/application.js
@@ -0,0 +1,4 @@
+"use strict";
+
+var tagline = document.getElementById('tagline');
+tagline.innerHTML = 'Added with JavaScript and Babel';
